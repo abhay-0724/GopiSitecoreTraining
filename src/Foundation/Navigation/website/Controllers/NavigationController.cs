fdae849d4e5bd1e6f3779a0c2d478ee46d929b30
abhay-0724 <abhay.patel4@cognizant.com>
2024-05-13 12:13:21 +0530

    Student components along with serialized Sitecore items

---
+++ src/Foundation/Navigation/website/Controllers/NavigationController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GopiSitecoreTraining.Foundation.Navigation.Controllers
+{
+    public class NavigationController : Controller
+    {
+        // GET: Navigation
+        public ActionResult GetNavigationItems()
+        {
+            var rootPath = Sitecore.Context.Site.RootPath;
+            var homeItem = Sitecore.Context.Database.GetItem(rootPath);
+            return View();
+        }
+    }
+}
